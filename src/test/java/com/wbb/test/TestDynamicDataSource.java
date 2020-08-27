5a97e2e26db31f716567843c4c1c0f375ed20fb8
Albert-WuBinBin <469548738@qq.com>
2020-08-27 16:55:32 +0800

    Update TestDynamicDataSource.java

--- src/test/java/com/wbb/test/TestDynamicDataSource.java
+++ src/test/java/com/wbb/test/TestDynamicDataSource.java
@@ -1,50 +1,50 @@
 package com.wbb.test;
 
-import javax.annotation.Resource;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.wbb.annotation.DynamicRoutingDataSource;
 import com.wbb.bean.Cost;
 import com.wbb.dataSource.MultiDataSource;
 import com.wbb.mapper.DataSourceMapper;
 import com.wbb.service.CostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * 动态切换数据源方法一
+ *
  * @author wbb
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring-mvc.xml",
-		"classpath:spring-mybatis.xml" })
+@ContextConfiguration(locations = {"classpath:spring-mvc.xml",
+        "classpath:spring-mybatis.xml"})
 public class TestDynamicDataSource {
 
-	@Resource
-	CostService costService;
-	@Resource
-	DataSourceMapper dataSourceMapper;
-	
-	/**
-	 * 使用工具类切换数据源
-	 */
-	@Test
-	public void test(){
-		Cost cost = new Cost(100);
-		costService.insert(cost);
-		MultiDataSource.setDataSourceKey("dataSource2");
-		costService.insert(cost);
-		MultiDataSource.toDefault();
-		costService.insert(cost);
-	}
-	
-	/**
-	 * 注解切换数据源
-	 */
-	@Test
-	public void test2(){
-		costService.insert2(new Cost(1000));
-	}
+    @Autowired
+    CostService costService;
+    @Autowired
+    DataSourceMapper dataSourceMapper;
+
+    /**
+     * 使用工具类切换数据源
+     */
+    @Test
+    public void test() {
+        Cost cost = new Cost(100);
+        costService.insert(cost);
+        MultiDataSource.setDataSourceKey("dataSource2");
+        costService.insert(cost);
+        MultiDataSource.toDefault();
+        costService.insert(cost);
+    }
+
+    /**
+     * 注解切换数据源
+     */
+    @Test
+    @DynamicRoutingDataSource("dataSource2")
+    public void test2() {
+        costService.insert2(new Cost(1000));
+    }
 }
